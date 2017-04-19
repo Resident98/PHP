423f9ea2c9fecac13414435f591b50412e8210db
Resident98 <bartekkupka222@gmail.com>
2017-04-19 14:47:27 +0200

    BMI

---
+++ Zadanie 2 - BMI/bmi.php
@@ -0,0 +1,38 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+	$waga=$_POST["waga"];
+	$wzrost=$_POST["wzrost"];
+	$BMI=$waga/($wzrost*$wzrost);
+	if($BMI<16.0)
+	{
+		print("Wygłodzenie");
+	}else if($BMI>=16.0&&$BMI<17.0)
+	{
+		print("Wychudzenie");
+	}else if($BMI>=17.0&&$BMI<18.5)
+	{
+		print("Niedowaga");
+	}else if($BMI>=18.5&&$BMI<25.0)
+	{
+		print("Waga prawidłowa");
+	}else if($BMI>=25.0&&$BMI<30.0)
+	{
+		print("Nadwaga");
+	}else if($BMI>=30.0&&$BMI<35.0)
+	{
+		print("I stopień otyłości");
+	}else if($BMI>=35.0&&$BMI<40.0)
+	{
+		print("II stopień otyłości");
+	}else if($BMI>=40.0)
+	{
+		print("III stopień otyłości (otyłość skrajna)");
+	}
+?>
+ </body>
+</html>
---
+++ Zadanie 2 - BMI/index.php
@@ -0,0 +1,13 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	<form action="bmi.php" method="post">
+	Podaj waga w kg: <input type="text" name="waga"><br>
+	Podaj wzrost w m: <input type="text" name="wzrost"><br>
+	<input type="submit">
+	</form>
+ </body>
+</html>
