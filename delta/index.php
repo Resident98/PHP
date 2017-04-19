58478c7eb7b9f2874fa5dbace345f9ba5699584a
Resident98 <bartekkupka222@gmail.com>
2017-04-19 14:21:06 +0200

    Delta

---
+++ delta/index.php
@@ -0,0 +1,15 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	Delta
+	<form action="delta.php" method="post">
+	Wpsiz a: <input type="Number" name="a"><br>
+	Wpsiz b: <input type="Number" name="b"><br>
+	Wpsiz c: <input type="Number" name="c"><br>
+	<input type="submit">
+	</form>
+ </body>
+</html>
