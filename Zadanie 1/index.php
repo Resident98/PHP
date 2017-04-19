aa906d3ebb5f364ddb68e2e3a254554ef4191926
Resident98 <bartekkupka222@gmail.com>
2017-04-19 14:29:07 +0200

    Hello world!

---
+++ Zadanie 1/index.php
@@ -0,0 +1,12 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	<form action="welcome.php" method="post">
+	Imię: <input type="text" name="name"><br>
+	<input type="submit">
+	</form>
+ </body>
+</html>
---
+++ Zadanie 1/welcome.php
@@ -0,0 +1,12 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ Witaj
+ <?php 
+	echo $_POST["name"];
+?>
+ </body>
+</html>
